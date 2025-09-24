8bffa7b7b6f188d4c204d05cc9384450b1c79c43
NK <1219176085@qq.com>
2025-09-24 14:12:47 +0800

    Update diy.sh
    添加 集客 AC OpenWRT 插件

--- diy.sh
+++ diy.sh
@@ -18,3 +18,4 @@ echo 'src-git UA3F https://github.com/SunBK201/UA3F.git' >>feeds.conf.default
 #git clone https://github.com/kenzok8/small.git package/small
 #git clone https://github.com/SunBK201/UA3F.git package/UA3F
 git clone https://github.com/stevenjoezhang/luci-app-adguardhome.git package/ADGH
+git clone https://github.com/lwb1978/openwrt-gecoosac.git package/GECOOSAC
